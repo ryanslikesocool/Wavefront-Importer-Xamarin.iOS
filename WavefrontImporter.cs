d59473eac72408e2ac3b07476568902062eacd4b
ryan <ryanjboyer@gmail.com>
2019-03-10 01:35:59 -0700

    Rewrote from the ground up with support for n-gons
    Also eliminated a pesky little warning relating to the number of elements in the mesh

--- WavefrontImporter.cs
+++ WavefrontImporter.cs
@@ -5,136 +5,120 @@ using System.Linq;
 using Foundation;
 using System.IO;
 
-public static class WavefrontImporter {
-    public static SCNGeometry ImportOBJ(string path) {
-        string resourcesPath = NSBundle.MainBundle.ResourcePath;
-        string[] fileContents = File.ReadLines(resourcesPath + path).ToArray();
+public class WavefrontImporter {
+    string[] vertexLines;
+    string[] normalLines;
+    string[] indexLines;
 
-        int[] vertexIndices = OBJVertexIndices(fileContents);
-        int[] normalIndices = OBJNormalIndices(fileContents);
-        SCNVector3[] vertices = OBJExtendedVertices(OBJVertices(fileContents), vertexIndices);
-        SCNVector3[] normals = OBJExtendedNormals(OBJNormals(fileContents), normalIndices);
+    int polygonCount = 0;
+
+    public SCNGeometry ImportOBJ(string path) {
+        string[] fileContents = File.ReadLines(path).ToArray();
+
+        GetLines(fileContents);
+        polygonCount = indexLines.Length;
+
+        int[] vertexIndexPrefix = SplitIndices(indexLines).Item1;
+        int[] vertexIndices = SplitIndices(indexLines).Item2;
+        int[] normalIndices = SplitIndices(indexLines).Item3;
+
+        SCNVector3[] vertexPoints = LinesToPoints(vertexLines);
+        SCNVector3[] normalPoints = LinesToPoints(normalLines);
+
+        SCNVector3[] vertices = OrderPoints(vertexPoints, vertexIndices);
+        SCNVector3[] normals = OrderPoints(normalPoints, normalIndices);
+
+        vertexIndices = ReorderIndices(vertexIndexPrefix, vertexIndices);
 
         NSData vertexIndexData = IndexData(vertexIndices);
-        NSData normalIndexData = IndexData(normalIndices);
 
         SCNGeometrySource vertexSource = SCNGeometrySource.FromVertices(vertices);
         SCNGeometrySource normalSource = SCNGeometrySource.FromNormals(normals);
-        SCNGeometryElement vertexElement = SCNGeometryElement.FromData(vertexIndexData, SCNGeometryPrimitiveType.Triangles, vertexIndices.Length, sizeof(int));
-        SCNGeometryElement normalElement = SCNGeometryElement.FromData(normalIndexData, SCNGeometryPrimitiveType.Triangles, normalIndices.Length, sizeof(int));
+        SCNGeometryElement vertexElement = SCNGeometryElement.FromData(vertexIndexData, SCNGeometryPrimitiveType.Polygon, polygonCount, sizeof(int));
 
-        return SCNGeometry.Create(new[] { vertexSource, normalSource }, new[] { vertexElement, normalElement });
+        return SCNGeometry.Create(new[] { vertexSource, normalSource }, new[] { vertexElement });
     }
 
-    public static SCNVector3[] OBJVertices(string[] file) {
-        List<SCNVector3> returnPoints = new List<SCNVector3>();
+    void GetLines(string[] file) {
+        List<string> vLines = new List<string>();
+        List<string> nLines = new List<string>();
+        List<string> iLines = new List<string>();
 
-        foreach (string line in file) {
-            if (line.StartsWith("v ", StringComparison.Ordinal)) {
-                string trim = line.TrimStart('v', ' ');
-                string[] points = trim.Split(" ");
-
-                returnPoints.Add(new SCNVector3(float.Parse(points[0]), float.Parse(points[1]), float.Parse(points[2])));
+        for (int i = 0; i < file.Length; i++) {
+            if (file[i].StartsWith("v ", StringComparison.Ordinal)) {
+                string trim = file[i].TrimStart('v', ' ');
+                vLines.Add(trim);
+            } else if (file[i].StartsWith("vn ", StringComparison.Ordinal)) {
+                string trim = file[i].TrimStart('v', 'n', ' ');
+                nLines.Add(trim);
+            } else if (file[i].StartsWith("f ", StringComparison.Ordinal)) {
+                string trim = file[i].TrimStart('f', ' ');
+                iLines.Add(trim);
             }
         }
-     
+
+        vertexLines = vLines.ToArray();
+        normalLines = nLines.ToArray();
+        indexLines = iLines.ToArray();
+    }
+
+    Tuple<int[], int[], int[]> SplitIndices(string[] lines) {
+        List<int> vertexIndicesPrefix = new List<int>();
+        List<int> returnVertexIndices = new List<int>();
+        List<int> returnNormalIndices = new List<int>();
+
+        for (int i = 0; i < lines.Length; i++) {
+            string trim = lines[i].TrimStart('f', ' ');
+            string[] indices = trim.Split(" ");
+
+            vertexIndicesPrefix.Add(indices.Length);
+
+            for (int j = 0; j < indices.Length; j++) {
+                returnVertexIndices.Add(int.Parse(indices[j].Split("//")[0]) - 1);
+                returnNormalIndices.Add(int.Parse(indices[j].Split("//")[1]) - 1);
+            }
+        }
+        
+        return new Tuple<int[], int[], int[]>(vertexIndicesPrefix.ToArray(), returnVertexIndices.ToArray(), returnNormalIndices.ToArray());
+    }
+
+    SCNVector3[] LinesToPoints(string[] lines) {
+        List<SCNVector3> returnPoints = new List<SCNVector3>();
+
+        for (int i = 0; i < lines.Length; i++) {
+            string[] parts = lines[i].Split(" ");
+            SCNVector3 point = new SCNVector3(float.Parse(parts[0]), float.Parse(parts[1]), float.Parse(parts[2]));
+            returnPoints.Add(point);
+        }
+
         return returnPoints.ToArray();
     }
 
-    public static SCNVector3[] OBJExtendedVertices(SCNVector3[] vertices, int[] indices) {
-        List<SCNVector3> returnPoints = new List<SCNVector3>();
+    SCNVector3[] OrderPoints(SCNVector3[] points, int[] indices) {
+        SCNVector3[] returnPoints = new SCNVector3[indices.Length];
 
         for (int i = 0; i < indices.Length; i++) {
-            returnPoints.Add(vertices[indices[i]]);
+            returnPoints[i] = points[indices[i]];
         }
 
-        return returnPoints.ToArray();
+        return returnPoints;
     }
 
-    public static SCNVector3[] OBJNormals(string[] file) {
-        List<SCNVector3> returnNormals = new List<SCNVector3>();
+    int[] ReorderIndices(int[] prefix, int[] indices) {
+        int[] returnIndices = new int[prefix.Length + indices.Length];
 
-        foreach (string line in file) {
-            if (line.StartsWith("vn ", StringComparison.Ordinal)) {
-                string trim = line.TrimStart('v', 'n', ' ');
-                string[] points = trim.Split(" ");
-
-                returnNormals.Add(SCNVector3.Normalize(new SCNVector3(float.Parse(points[0]), float.Parse(points[1]), float.Parse(points[2]))));
-            }
+        for (int i = 0; i < prefix.Length; i++) {
+            returnIndices[i] = prefix[i];
         }
-
-        return returnNormals.ToArray();
-    }
-
-    public static SCNVector3[] OBJExtendedNormals(SCNVector3[] normals, int[] indices) {
-        List<SCNVector3> returnPoints = new List<SCNVector3>();
 
         for (int i = 0; i < indices.Length; i++) {
-            returnPoints.Add(normals[indices[i]]);
+            returnIndices[i + prefix.Length] = i;
         }
 
-        return returnPoints.ToArray();
+        return returnIndices;
     }
 
-    public static int[] OBJVertexIndices(string[] file) {
-        List<int> returnIndices = new List<int>();
-
-        foreach (string line in file) {
-            if (line.StartsWith("f", StringComparison.Ordinal)) {
-                string trim = line.TrimStart('f', ' ');
-                string[] indices = trim.Split(" ");
-
-                for (int i=0; i < indices.Length; i++) {
-                    indices[i] = indices[i].Split("//")[0];
-                }
-
-                for (int i = 0; i < 3; i++) {
-                    returnIndices.Add(int.Parse(indices[i]) - 1);
-                }
-
-                if (indices.Length == 4) {
-                    returnIndices.Add(int.Parse(indices[0]) - 1);
-
-                    for (int i = 2; i < 4; i++) {
-                        returnIndices.Add(int.Parse(indices[i]) - 1);
-                    }
-                }
-            }
-        }
-
-        return returnIndices.ToArray();
-    }
-
-    public static int[] OBJNormalIndices(string[] file) {
-        List<int> returnIndices = new List<int>();
-
-        foreach (string line in file) {
-            if (line.StartsWith("f", StringComparison.Ordinal) && line.Contains("//")) {
-                string trim = line.TrimStart('f', ' ');
-                string[] indices = trim.Split(" ");
-
-                for (int i = 0; i < indices.Length; i++) {
-                    indices[i] = indices[i].Split("//")[1];
-                }
-
-                for (int i = 0; i < 3; i++) {
-                    returnIndices.Add(int.Parse(indices[i]) - 1);
-                }
-
-                if (indices.Length == 4) {
-                    returnIndices.Add(int.Parse(indices[0]) - 1);
-
-                    for (int i = 2; i < 4; i++) {
-                        returnIndices.Add(int.Parse(indices[i]) - 1);
-                    }
-                }
-            }
-        }
-
-        return returnIndices.ToArray();
-    }
-
-    public static NSData IndexData(int[] indices) {
+    public NSData IndexData(int[] indices) {
         byte[][] returnIndexArray = new byte[indices.Length][];
         for (int i = 0; i < indices.Length; i++) {
             returnIndexArray[i] = BitConverter.GetBytes(indices[i]);
@@ -143,4 +127,9 @@ public static class WavefrontImporter {
 
         return returnIndexData;
     }
+
+    //Unnecessary, but here for reference
+    /*public SCNVector3 CalculateNormal(SCNVector3 a, SCNVector3 b, SCNVector3 c) {
+        return SCNVector3.Normalize(SCNVector3.Cross(b - a, c - a));
+    }*/
 }
